ecabec5a9fb0d69522b2b09d17bcdd7a7da10dab
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-03-26 14:38:24 +0400

    add error message when news not found

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -10,9 +10,9 @@ class NewsItemsController < ApplicationController
   def find
     item = NewsItem.find_by_url_key(params[:key])
     if item
-      redirect_to news_path item
+      redirect_to news_path(item), status: 301
     else
-      redirect_to news_index_path
+      redirect_to news_index_path, flash: { error: "Page doesn't exist." }
     end
   end
 end
