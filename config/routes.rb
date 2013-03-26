7d0c877023b5b90a348118337384a1db5f7e2852
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-03-26 14:37:12 +0400

    routes refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Shorturls::Application.routes.draw do
   root :to => 'news_items#index'
 
-  get '/n/:key', to: 'news_items#find', as: :short_news_url
+  get '/n/:key' => 'news_items#find', as: :short_news_url
 
   resources :news, controller: 'news_items', only: [:index, :show]
 end
