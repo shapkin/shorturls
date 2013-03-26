2067319a34e283538e8b390afc203b46c35783fd
Alexander Shapkin <as.zgroupsoft@gmail.com>
2013-03-26 15:02:17 +0400

    fix url_key generator

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -10,7 +10,7 @@ class NewsItem < ActiveRecord::Base
   private
 
     def generate_url_key
-      key = Digest::MD5.hexdigest("#{id}#{Time.now.utc}").slice(0..7)
+      key = Digest::MD5.hexdigest("#{title}#{Time.now.to_f}").slice(0..7)
       if NewsItem.find_by_url_key(key)
         generate_url_key
       else
